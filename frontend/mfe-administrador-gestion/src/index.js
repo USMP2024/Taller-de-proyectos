5b9fac41b10dc2cca229ea19042e859495449232
lMiguelCa <luis_castro15@usmp.pe>
2024-05-25 17:50:55 -0500

    add index

---
+++ frontend/mfe-administrador-gestion/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Toaster} from "react-hot-toast";
+import './index.css';
+import { AppRouter } from './router/AppRouter';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+      <AppRouter />
+      <Toaster/>
+
+  </React.StrictMode>
+);
+
