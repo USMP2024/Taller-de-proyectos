b47908c95bf385d6bd49a0251c15b7889ee0edcd
lMiguelCa <luis_castro15@usmp.pe>
2024-05-25 17:46:29 -0500

    header Administrador Usuario

---
+++ frontend/mfe-administrador-gestion/src/components/GestionUsuarios/header/Header.jsx
@@ -0,0 +1,32 @@
+import React, { useState} from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import "./Header.css";
+
+export const Header = () => {  
+    const[menuOpen, setMenuOpen] = useState(false)
+    return (
+    <nav>
+        <Link to="/" className='title'>Inti Pacha Artes - Admin</Link>
+
+        <div className='menu' onClick={() =>{
+            setMenuOpen(!menuOpen);
+        }}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        
+        <ul className={menuOpen? "open" : ""}>
+            <li>
+                <NavLink to="/">Gestionar Usuarios</NavLink>
+            </li>
+            {/* <li>
+                <NavLink to="/Solicitud">Gestionar Solicitudes</NavLink>
+            </li>
+            <li>
+                <NavLink to="">Gestionar Devoluciones</NavLink>
+            </li> */}
+        </ul>
+    </nav>
+    )
+}
