ed3443000a93ed2fc7d5e6930ceea1ce1fc541b6
AValentinoGPereda <angel_garcia3@usmp.pe>
2024-05-26 17:36:24 -0500

    Lambda buscar preguntas frecuentes v1

---
+++ backend/lambda-buscar-preguntas-frecuentes/index.js
@@ -0,0 +1,45 @@
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event) => {
+    const categories = ["Comenzar", "Revisión del contenido", "Su cartera", "Centro legal"];
+    const results = {};
+
+    for (const category of categories) {
+        const params = {
+            TableName: 'dynamo-preguntas-frecuentes',
+            IndexName: 'idPregunta',  // Assuming there is a secondary index on categoriaPregunta
+            KeyConditionExpression: '#cat = :category',
+            ExpressionAttributeNames: {
+                '#cat': 'categoria'
+            },
+            ExpressionAttributeValues: {
+                ':category': category
+            }
+        };
+
+        try {
+            const response = await dynamodb.query(params).promise();
+            const items = response.Items;
+
+            // Sort the items by nroMeGustas in descending order
+            items.sort((a, b) => b.nroMeGustas - a.nroMeGustas);
+
+            // Get the top 3 items
+            const topItems = items.slice(0, 3);
+            results[category] = topItems;
+        } catch (error) {
+            console.error(`Error querying for category ${category}:`, error);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: `Error querying for category ${category}` })
+            };
+        }
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(results)
+    };
+};
