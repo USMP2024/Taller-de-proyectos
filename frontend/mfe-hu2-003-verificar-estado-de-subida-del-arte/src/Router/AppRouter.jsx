0e927e7663a30e30775091f29a26242bd9370d95
lMiguelCa <luis_castro15@usmp.pe>
2024-06-01 17:08:31 -0500

    add AppRouter

---
+++ frontend/mfe-hu2-003-verificar-estado-de-subida-del-arte/src/Router/AppRouter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import VerificarImagenes from '../Componentes/VerificarImagenes/VerificarImagenes'
+import VerificarVideos from '../Componentes/VerificarVideos/VerificarVideos'
+
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+            <Route path="/VerificarImagenes" element={<VerificarImagenes/>}/>
+            <Route path="/VerificarVideos" element={<VerificarVideos/>}/>
+        </Routes>
+
+    </>
+
+  )
+}
+
+
+
