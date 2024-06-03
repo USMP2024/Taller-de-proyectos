8846bc83a2f7ed9451bf0f74b3ea7c129049a07a
Lnavarro21 <luis_navarro1@usmp.pe>
2024-06-03 12:20:43 -0500

    Ultimos cambios

--- frontend/mfe-carrito-de-compras/src/root.component.js
+++ frontend/mfe-carrito-de-compras/src/root.component.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return <App />;
 }
+
+
+
