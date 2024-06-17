eb2df99ab19c6458c70fc5e5e5dc5b6d4a6cb388
Jackguarniz <jack_guarniz@usmp.pe>
2024-06-17 11:12:00 -0500

    vista de sprint4

---
+++ frontend/mfe-finanzas-contribuidor/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <Card.Title>Resumen de ingresos</Card.Title>
+        
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Header;
